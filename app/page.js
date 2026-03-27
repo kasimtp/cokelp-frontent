e89b634255afaee0995cefd4e7d2c1e051f1b404
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2026-03-27 08:42:06 +0530

    start

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@base-ui/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
     <h1 className="text-red-500">H;ldcome</h1>
+    <Button variant="secondary">Seconscscscdary</Button>
    </div>
   );
 }
---
+++ components/ui/Header.jsx
@@ -0,0 +1,32 @@
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className=" fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md x-50 supports-backdrop-filter:bg-stone-50/60 flex justify-end items-center p-4 gap-4 h-16">
+            <nav className='container bg-red-000 mx-auto px-4 h-16 flex item-center justify-between'>
+                logo
+                <div className='text-red-600'>nav</div>
+                <div className='flex item-center space-x-4'>
+                    <Show when="signed-out">
+                        <SignInButton>
+                            <button className="text-orange-500  rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                sign in
+                            </button></SignInButton> 
+                        <SignUpButton>
+                            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                Sign Up
+                            </button>
+                        </SignUpButton>
+                    </Show>
+                    <Show when=" signed-in">
+                        <UserButton />
+                    </Show>
+                </div>
+
+            </nav>
+        </header>
+
+        // 26:31
+    )
+}
